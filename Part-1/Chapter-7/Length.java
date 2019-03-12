528a1abb9136b4f81ab5ec808a1142fb1ed588f6
minhaj <minhajulalam.cse@gmail.com>
2019-03-12 22:56:46 +0600

    The program demonstrate the length array member.

---
+++ Part-1/Chapter-7/Length.java
@@ -0,0 +1,13 @@
+// This program demonstrates the length array member.
+
+class Length{
+	public static void main(String args[]){
+		int a1[] = new int[10];
+		int a2[] = {3,2,4,1,5,33,44,-10};
+		int a3[] = {4,3,2,1};
+
+		System.out.println("length of a1 is "+a1.length);
+		System.out.println("length of a1 is "+a2.length);
+		System.out.println("length of a1 is "+a3.length);		
+	}
+}
