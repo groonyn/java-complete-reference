5fb0472d52d4505dc9c158aff03c2ff372a0fcd6
minhaj <minhajulalam.cse@gmail.com>
2019-04-22 00:28:20 +0600

    Construcct sub string.

---
+++ Part-2/Chapter-17/SubString.java
@@ -0,0 +1,9 @@
+// Construct sub string
+class SubString{
+	public static void main(String args[]){
+		char c[] = {'a','b','c','d','e','f'};
+		String s1 = new String(c,2,3);
+
+		System.out.println(s1);
+	}
+}
