ad095d15b561604e039d3da88d6acd4cb8a724da
minhaj <minhajulalam.cse@gmail.com>
2019-04-21 23:34:50 +0600

    Modified

--- Part-1/Chapter-2/Example.java
+++ Part-1/Chapter-2/Example.java
@@ -1,10 +1,11 @@
 /*
 	This is a simple Java program.
-	Call this ifle "Example.java".
+	Call this file "Example.java".
 */
+
 class Example{
-	//Your program begins with a call to main().
+	//Your program begins with a call to main()
 	public static void main(String args[]){
-		System.out.println("This is a simple Java program.")
+		System.out.println("This is a simple Java program.");
 	}
 }
